ff1864b0ea04212451cbcf7abbe402f8f92effe3
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-07-04 08:29:50 +0000

    Deploying to gh-pages from @ Natalka333/js-11-selfeducation@fcea5ea9565538649bc7f60b485eee97dff34776 🚀

---
+++ example.f3e62d47.js
@@ -0,0 +1,2 @@
+function o(o){return Promise.resolve({apple:"яблоко",kiwi:"киви",strawberry:"клубника"}[o])}!async function(){console.log(await o("apple"));const e=await o("kiwi");console.log(e)}();
+//# sourceMappingURL=example.f3e62d47.js.map
