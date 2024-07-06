aed0e682365d8135fb38be1515ad7e5781fe930b
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-07-06 11:57:15 +0300

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,15 @@ import pixabayApi from './pixabayApi'
 import { renderCardImage } from "./renderCard";
 import { fetchImages } from "./pixabayApi";
 
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.btn-load');
+
+let page = 1;
+let query = '';
+
+formEl.addEventListener('submit', handlechooseImage);
+
+function handlechooseImage(event) {
+    event.preventDefault();
+}
